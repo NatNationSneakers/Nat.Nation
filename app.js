987e258985521239aaa16245d724f01683b36b90
NatNationSneakers <natnation1812@gmail.com>
2026-04-06 14:37:53 -0600

    act pag 3.0

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 function entrar() {
-    document.getElementById("inicio").style.display = "none";
-    document.getElementById("tienda").style.display = "block";
+    const contenido = document.getElementById("contenido");
+
+    // 🔥 activa el zoom
+    contenido.classList.add("zoom-out");
+
+    setTimeout(() => {
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("tienda").style.display = "block";
+
+        // 🔄 quitamos zoom para la siguiente vez
+        contenido.classList.remove("zoom-out");
+    }, 300); // ⏱ mismo tiempo que tu CSS
 }
 
 function regresar() {
@@ -19,16 +29,41 @@ function actualizarCarrito() {
 
     carrito.forEach((item, index) => {
         lista.innerHTML += `
-            <li>
-                ${item}
-                <button onclick="eliminar(${index})">❌</button>
-            </li>
-        `;
+   <li class="item-carrito">
+    
+    <div class="contenido-item">
+        <img src="${item.imagen}" class="img-carrito">
+
+        <div class="info">
+            <p class="nombre">${item.nombre}</p>
+            <p class="talla">Talla: ${item.talla}</p>
+            <p class="precio">${item.precio}</p>
+           <div class="control-cantidad">
+    <button onclick="cambiarCantidad(${index}, -1)">➖</button>
+    <span>${item.cantidad}</span>
+    <button onclick="cambiarCantidad(${index}, 1)">➕</button>
+</div>
+        </div>
+    </div>
+
+    <button class="btn-eliminar" onclick="eliminar(${index})">🗑️</button>
+
+</li>
+`;
     });
 }
 
 function eliminar(index) {
     carrito.splice(index, 1);
+    actualizarCarrito();
+}
+function cambiarCantidad(index, cambio) {
+    carrito[index].cantidad += cambio;
+
+    if (carrito[index].cantidad <= 0) {
+        carrito.splice(index, 1);
+    }
+
     actualizarCarrito();
 }
 
@@ -37,21 +72,39 @@ function Agregar(boton) {
     let producto = card.querySelector("h2").innerText;
     let talla = card.querySelector("select").value;
 
-    carrito.push(producto + " - " + talla);
+let precio = card.querySelector("p").innerText;
+let imagen = card.querySelector("img").src;
+
+ let existente = carrito.find(item => 
+    item.nombre === producto && item.talla === talla
+);
+
+if (existente) {
+    existente.cantidad++;
+} else {
+    carrito.push({
+        nombre: producto,
+        talla: talla,
+        precio: precio,
+        imagen: imagen,
+        cantidad: 1
+    });
+}
+
     actualizarCarrito();
 }
 
 
 window.onload = function () {
     setTimeout(() => {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("contenido").style.display = "block";
-    }, 2000);
-};
-
-window.onload = function () {
-    setTimeout(() => {
         document.getElementById("loader").classList.add("oculto");
         document.getElementById("contenido").classList.add("visible");
-    }, 2000);
+    }, 800);
 };
+
+function toggleCarrito() {
+    let panel = document.getElementById("panel-carrito");
+    panel.classList.toggle("abierto");
+}
+
+li.innerHTML = producto + ' <button class="btn-eliminar" onclick="eliminar(this)">🗑️</button>';
